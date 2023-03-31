307537eff32d216c13b1611a4348034ef7fda721
omid beigi <o.beygi@gmail.com>
2023-03-31 18:56:01 +0330

    tab navigation done

--- navigators/StackNav.js
+++ navigators/StackNav.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from '../screens/Login'
 import Home from '../screens/Home'
+import Tab from "./Tab";
 
 
 const StackNav = () => {
@@ -9,7 +10,7 @@ const StackNav = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Application" component={Home} />
+      <Stack.Screen name="Application" component={Tab} />
     </Stack.Navigator>
   );
 };
--- navigators/Tab.js
+++ navigators/Tab.js
@@ -1,14 +1,76 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  Entypo,
+  FontAwesome,
+  Feather,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import Home from "../screens/Home";
 
 const Tab = () => {
+  const TabNav = createBottomTabNavigator();
   return (
-    <View>
-      <Text>Tab</Text>
-    </View>
-  )
-}
+    <TabNav.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: { backgroundColor: "#000" },
+      }}
+    >
+      <TabNav.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <TabNav.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="search" size={size} color={color} />
+          ),
+        }}
+      />
+      <TabNav.Screen
+        name="Reels"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="video-collection" size={size} color={color} />
+          ),
+        }}
+      />
+      <TabNav.Screen
+        name="Shopping"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="shopping-bag" size={size} color={color} />
+          ),
+        }}
+      />
+      <TabNav.Screen
+        name="Profile"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              source={require("../assets/profile.jpeg")}
+              style={{ width: 30, height: 30, borderRadius: 15 }}
+            />
+          ),
+        }}
+      />
+    </TabNav.Navigator>
+  );
+};
 
-export default Tab
+export default Tab;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
